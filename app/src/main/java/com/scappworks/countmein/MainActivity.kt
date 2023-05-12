58d6f30766f650bdd5289bdd893f04bcb57e76cd
scappworks <scappworks@gmail.com>
2023-05-11 23:21:24 -0400

    init commit

---
+++ app/src/main/java/com/scappworks/countmein/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.scappworks.countmein
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val runningCountFormula = findViewById<TextView>(R.id.running_count_formula)
+        val runningCountFormulaArray = resources.getStringArray(R.array.running_count_formula)
+        val playerCountList = resources.getStringArray(R.array.player_count_array).toList()
+        val playerCounterAdapter = ArrayAdapter(this, R.layout.player_count, playerCountList)
+        val playerCountSpinner = findViewById<Spinner>(R.id.player_spinner)
+        playerCountSpinner.adapter = playerCounterAdapter
+        playerCountSpinner.setSelection(0)
+        val deckCountList = resources.getStringArray(R.array.deck_count_array)
+        val deckCountAdapter = ArrayAdapter(this, R.layout.player_count, deckCountList)
+        val deckCountSpinner = findViewById<Spinner>(R.id.deck_spinner)
+        deckCountSpinner.adapter = deckCountAdapter
+        deckCountSpinner.setSelection(deckCountList.size - 1)
+
+
+        runningCountFormulaArray.forEachIndexed {index, it ->
+            if (index == 0) {
+                runningCountFormula.text = ""
+                runningCountFormula.append("\n" + it)
+            }
+            else {
+                runningCountFormula.append("\n" + it)
+            }
+        }
+    }
+}
