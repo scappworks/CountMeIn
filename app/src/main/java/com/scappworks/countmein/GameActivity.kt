8c49609987c8e2df2c7b36c67b487590177187b7
scappworks <scappworks@gmail.com>
2023-05-18 07:35:43 -0400

    create deck building function. needs to be redone correctly to build multiples and face cards

--- app/src/main/java/com/scappworks/countmein/GameActivity.kt
+++ app/src/main/java/com/scappworks/countmein/GameActivity.kt
@@ -13,5 +13,23 @@ class GameActivity : AppCompatActivity() {
         val extras = intent.extras
         val playerCount = extras!!.getString("playerCount")!!.toInt()
         val deckCount = extras.getString("deckCount")!!.toInt()
+        val shoe:MutableList<Any> = createDeck()
+
+        Log.d("DECK", shoe.toString())
+    }
+
+    private fun createDeck(): MutableList<Any> {
+        val builtDeck: MutableList<Any> = arrayListOf()
+        for(i in 1..52) {
+            if (i == 1 || i == 11) {
+                builtDeck.add("A")
+            }
+            else if(i >= 10) {
+                builtDeck.add(i)
+            }
+
+        }
+
+        return builtDeck
     }
 }
