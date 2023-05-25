916903d27d13cdb4acf4a9eebdafc9ffc0de340e
scappworks <scappworks@gmail.com>
2023-05-24 21:13:11 -0400

    move variables to a data class (GameVariables.kt) for better control and organization

--- app/src/main/java/com/scappworks/countmein/GameActivity.kt
+++ app/src/main/java/com/scappworks/countmein/GameActivity.kt
@@ -16,63 +16,13 @@ class GameActivity : AppCompatActivity() {
         val extras = intent.extras
         val playerCount = extras!!.getString("playerCount")!!.toInt()
         val deckCount = extras.getString("deckCount")!!.toInt()
-        // Variables for this activity
-        var shoe:List<String> = createDeck(deckCount)
-        var remainingShoe = shoe
-        var remainingDecks = deckCount
-        var remainingCardCount = remainingShoe.count()
-        var runningCount = 0
-        var trueCount = runningCount / remainingDecks
-        var playerHands = drawHands(playerCount, remainingShoe)
 
-        playerHands.forEach {
-            Log.i("BIGTEST", it.firstCard)
-            Log.i("BIGTEST2", it.secondCard)
+        val gameVariables = GameVariables(playerCount, deckCount)
+
+
+        gameVariables.playerHands.forEach {
+            Log.i("TEST", it.firstCard)
+            Log.i("TEST", it.secondCard)
         }
-    }
-
-    class PlayerHand(firstCardIn:String, secondCardIn:String) {
-        val firstCard = firstCardIn
-        val secondCard = secondCardIn
-    }
-
-    private fun subtractShoe(playerCount:Int, shoeCount:Int): Int {
-        return shoeCount - (playerCount * 2)
-    }
-
-    private fun drawHands(playerCount:Int, shoe:List<String>): List<PlayerHand> {
-        val hands = mutableListOf<PlayerHand>()
-        val tempShoe = shoe.toMutableList()
-
-        for(i in 1..playerCount) {
-            val hand = PlayerHand(tempShoe[0], tempShoe[1])
-            hands.add(hand)
-            tempShoe.removeAt(0)
-            tempShoe.removeAt(0)
-        }
-
-        return hands.toList()
-    }
-
-    private fun createDeck(deckCount:Int): List<String> {
-        val suits = arrayOf("Hearts", "Spades", "Clubs", "Diamonds")
-        val faceCards = arrayOf("King", "Queen", "Jack", "Ace")
-        val builtDeck: MutableList<String> = mutableListOf()
-
-        for(d in 1..deckCount) {
-            suits.forEach {
-                // Add all 2 - 10 cards per suit
-                for (i in 2..10) {
-                    builtDeck.add("$i $it")
-                }
-
-                // Add all face cards per suit
-                faceCards.forEach { fc ->
-                    builtDeck.add("$fc $it")
-                }
-            }
-        }
-
-        return builtDeck.shuffled()
     }
 }
---
+++ app/src/main/java/com/scappworks/countmein/GameVariables.kt
@@ -0,0 +1,51 @@
+package com.scappworks.countmein
+
+data class GameVariables(val playerCount:Int, val deckCount:Int) {
+    var shoe:List<String> = createDeck(deckCount)
+    var remainingDecks = deckCount
+    var remainingCardCount = shoe.count()
+    var runningCount = 0
+    var trueCount = runningCount / remainingDecks
+    var playerHands = drawHands(playerCount, shoe)
+}
+
+class PlayerHand(firstCardIn:String, secondCardIn:String) {
+    val firstCard = firstCardIn
+    val secondCard = secondCardIn
+}
+
+private fun drawHands(playerCount:Int, shoe:List<String>): List<PlayerHand> {
+    val hands = mutableListOf<PlayerHand>()
+    val tempShoe = shoe.toMutableList()
+
+    for(i in 1..playerCount) {
+        val hand = PlayerHand(tempShoe[0], tempShoe[1])
+        hands.add(hand)
+        tempShoe.removeAt(0)
+        tempShoe.removeAt(0)
+    }
+
+    return hands.toList()
+}
+
+private fun createDeck(deckCount:Int): List<String> {
+    val suits = arrayOf("Hearts", "Spades", "Clubs", "Diamonds")
+    val faceCards = arrayOf("King", "Queen", "Jack", "Ace")
+    val builtDeck: MutableList<String> = mutableListOf()
+
+    for(d in 1..deckCount) {
+        suits.forEach {
+            // Add all 2 - 10 cards per suit
+            for (i in 2..10) {
+                builtDeck.add("$i $it")
+            }
+
+            // Add all face cards per suit
+            faceCards.forEach { fc ->
+                builtDeck.add("$fc $it")
+            }
+        }
+    }
+
+    return builtDeck.shuffled()
+}
